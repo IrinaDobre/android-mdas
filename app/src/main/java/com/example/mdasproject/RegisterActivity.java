3020f98968dedccd55964e6c214992de7b9b16d9
Dobre Darius <dadobre@deloittece.com>
2020-11-11 22:23:36 +0200

    register changes

--- app/src/main/java/com/example/mdasproject/RegisterActivity.java
+++ app/src/main/java/com/example/mdasproject/RegisterActivity.java
@@ -3,12 +3,28 @@ package com.example.mdasproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etConfirmPassword;
+    private EditText etName;
+    private EditText etAdress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initData();
+    }
+
+    public void initData() {
+        etEmail = findViewById(R.id.editTextEmailAddressRegister);
+        etPassword = findViewById(R.id.editTextPasswordRegister);
+        etConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+        etName = findViewById(R.id.editTextName);
+        etAdress = findViewById(R.id.editTextAdress);
     }
 }
